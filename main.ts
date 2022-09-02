cbe0e152eef33b893077ee75fc1a69c1ebe47fd8
Rob Wilson <rob@rwilson.org.uk>
2022-09-02 16:22:30 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,18 +5,10 @@ function readTime () {
 }
 // Round to 3 dec places, and multiply by gain for attenuated inputs
 function makeReading () {
-    for (let index = 0; index <= Nloops; index++) {
-        ADC0 = "" + gain * _2decPlaces(ADS1115.readADC(0), 3) + ","
-    }
-    for (let index2 = 0; index2 <= Nloops; index2++) {
-        ADC1 = "" + gain * _2decPlaces(ADS1115.readADC(1), 3) + ","
-    }
-    for (let index3 = 0; index3 <= Nloops; index3++) {
-        ADC2 = "" + gain * _2decPlaces(ADS1115.readADC(2), 3) + ","
-    }
-    for (let index4 = 0; index4 <= Nloops; index4++) {
-        ADC3 = _2decPlaces(ADS1115.readADC(3), 3)
-    }
+    ADC0 = "" + _2decPlaces(ADS1115.readADC(0, 10) / scale0, 3) + ","
+    ADC1 = "" + _2decPlaces(ADS1115.readADC(1, 10) / scale1, 3) + ","
+    ADC2 = "" + _2decPlaces(ADS1115.readADC(2, 10) / scale2, 3) + ","
+    ADC3 = convertToText(_2decPlaces(ADS1115.readADC(3, 10), 3))
 }
 function resetReadings () {
     count = 0
@@ -40,17 +32,19 @@ function _2decPlaces (num: number, places: number) {
 }
 // Instant Reading
 input.onButtonPressed(Button.A, function () {
+    makeReading()
     serial.writeLine(ADC0)
     serial.writeLine(ADC1)
     serial.writeLine(ADC2)
-    serial.writeLine("" + (ADC3))
+    serial.writeLine(ADC3)
     radio.sendString(ADC0)
-    basic.pause(100)
+    basic.pause(sendDelay)
     radio.sendString(ADC1)
-    basic.pause(100)
+    basic.pause(sendDelay)
     radio.sendString(ADC2)
-    basic.pause(100)
-    radio.sendString("" + (ADC3))
+    basic.pause(sendDelay)
+    radio.sendString(ADC3)
+    basic.pause(sendDelay)
 })
 function setDate (text: string) {
     params = text.substr(2, text.length - 2)
@@ -68,15 +62,15 @@ function upload () {
     if (count > 0) {
         for (let index5 = 0; index5 <= count - 1; index5++) {
             radio.sendString("" + (dateTimeReadings[index5]))
-            basic.pause(100)
+            basic.pause(sendDelay)
             radio.sendString("" + (Vreadings0[index5]))
-            basic.pause(100)
+            basic.pause(sendDelay)
             radio.sendString("" + (Vreadings1[index5]))
-            basic.pause(100)
+            basic.pause(sendDelay)
             radio.sendString("" + (Vreadings2[index5]))
-            basic.pause(100)
+            basic.pause(sendDelay)
             radio.sendString("" + (Vreadings3[index5]))
-            basic.pause(100)
+            basic.pause(sendDelay)
         }
     }
 }
@@ -111,29 +105,38 @@ radio.onReceivedString(function (receivedString) {
 let params = ""
 let b = 0
 let a = 0
-let Vreadings3: number[] = []
+let Vreadings3: string[] = []
 let Vreadings2: string[] = []
 let Vreadings1: string[] = []
 let Vreadings0: string[] = []
 let dateTimeReadings: string[] = []
-let ADC3 = 0
+let ADC3 = ""
 let ADC2 = ""
 let ADC1 = ""
 let ADC0 = ""
 let dateTime = ""
 let time = ""
 let date = ""
-let gain = 0
+let scale2 = 0
+let scale1 = 0
+let scale0 = 0
+let sendDelay = 0
 let count = 0
-let Nloops = 0
 let command = ""
 let stringIn = ""
 stringIn = ""
 command = ""
 let oneMinute = 60000
-Nloops = 50
 count = 0
-gain = 3
+let gain = 3
+// Delay between sending Radio messages
+sendDelay = 500
+// Accurate scaling for attenuators
+scale0 = 0.3339
+// Accurate scaling for attenuators
+scale1 = 0.3301
+// Accurate scaling for attenuators
+scale2 = 0.3297
 ADS1115.setADDR(72)
 ADS1115.setFSR(FSR.V4)
 radio.setGroup(1)
@@ -141,7 +144,7 @@ resetReadings()
 makeReading()
 // TODO - add multi-minute loop
 loops.everyInterval(oneMinute, function () {
-    if (DS3231.minute() % 5 == 0) {
+    if (DS3231.minute() % 30 == 0) {
         readTime()
         dateTimeReadings.push(dateTime)
         makeReading()
