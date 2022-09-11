bd29ce40a89721cd8f52c1022c4a2ab516ed717d
Rob Wilson <rob@rwilson.org.uk>
2022-09-11 19:14:34 +0100

    handle rt

--- main.ts
+++ main.ts
@@ -99,7 +99,7 @@ radio.onReceivedString(function (receivedString) {
     if (command.compare("rt") == 0) {
         serial.writeLine("#readtime")
         readTime()
-        radio.sendString(dateTimeString)
+        radio.sendString("rt = " + dateTimeString)
     } else if (command.compare("st") == 0) {
         setTime(stringIn)
     } else if (command.compare("sd") == 0) {
